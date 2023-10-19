88e580c4bbebac666c95fba24f0599c921a8cb9d
Ibrahim Ali Hamim <87495140+IbrahimAliHamim@users.noreply.github.com>
2023-10-19 06:54:08 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JavaScript code as newer version
+
+let name = "Hamim";
+let age = 22;
+let isLoggedIn = true;
+
+// number => 2 to the power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol
+
+
+
+// object
+
+
+// console.log(typeof "Hitesh");
+// console.log(typeof age);
+
+// console.log("Typeof of null is - " + typeof null);
