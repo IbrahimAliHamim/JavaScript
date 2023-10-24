f0bdc49125cebf0da58f4278f611dd79724b65b4
Ibrahim Ali Hamim <87495140+IbrahimAliHamim@users.noreply.github.com>
2023-10-24 19:09:31 +0000

    String

---
+++ 01_basics/05_string.js
@@ -0,0 +1,60 @@
+const namE = "Ibrahim";
+const repoCount = 50;
+
+// console.log(name + repoCount + " Value")
+
+
+// ------------------------------------------
+
+// console.log(`My name is ${namE} and my repo count is ${repoCount}`);
+
+// ------------------------------------------
+
+// -----const strinG = new String('Ibrahim');
+
+// console.log(strinG[0]);
+
+// console.log(strinG.__proto__);
+
+// console.log(strinG.length)
+
+// console.log(strinG.toUpperCase());
+
+// console.log(strinG.charAt(3));
+// console.log(strinG.indexOf('r'));
+
+
+// -------------------------------------------
+
+// const strinG = new String('Ibrahim-ali');
+
+// const newStrinG = strinG.substring(0, 7);
+// console.log(newStrinG);
+
+// --------------
+
+// const new2StrinG = strinG.slice(-9, -4);
+// console.log(new2StrinG);
+
+
+// ----------------------------------
+
+// const triM = "       Ibrahim   ";
+// console.log(triM);
+// console.log(triM.trim());
+
+
+// -----------------------------------
+
+// const url = "https://ibrahimalihamim%50MERN";
+
+// console.log(url.replace('%50MERN', '-Url'));
+
+// console.log(url.includes('HItesh'));
+
+
+// ---------------------------------
+
+const strinG = new String('Ibrahim-Ali-Hamim');
+
+console.log(strinG.split('-'));
